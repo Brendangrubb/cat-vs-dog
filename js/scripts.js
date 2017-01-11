efcfce21c09582eadec93f3a3fd7beb368778076
brendan grubb <brendangrubb@gmail.com>
2017-01-11 12:57:09 -0800

    add (this).remove function to lists

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,18 @@ $("document").ready(function(){
 
   $("button#cat-btn").click(function() {
     $("ul#cat-response").prepend("<li>MeowHiss!</li>");
+
+    $("ul#cat-response").children("li").first().click(function() {
+      $(this).remove();
+    });
   })
 
   $("button#dog-btn").click(function() {
     $("ul#dog-response").prepend("<li>WoofBark!</li>");
 
+    $("ul#dog-response").children("li").first().click(function() {
+      $(this).remove();
+    })
 
   })
 
