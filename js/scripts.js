c25f42466cab1d9d9f24460fcab98365c2b32863
brendan grubb <brendangrubb@gmail.com>
2017-01-11 12:47:12 -0800

    add button functionality, styling

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$("document").ready(function(){
+
+  $("button#cat-btn").click(function() {
+    $("ul#cat-response").prepend("<li>MeowHiss!</li>");
+  })
+
+  $("button#dog-btn").click(function() {
+    $("ul#dog-response").prepend("<li>WoofBark!</li>");
+
+
+  })
+
+
+
+
+
+
+
+})
